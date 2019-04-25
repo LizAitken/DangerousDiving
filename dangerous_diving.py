e2ee591002b9a6fdeb698a355b65175a472d8cbd
RJ Salamanca <salamancaronito@gmail.com>
2019-04-25 13:45:37 -0400

    updated class methods

--- dangerous_diving.py
+++ dangerous_diving.py
@@ -5,28 +5,55 @@ game_width = 1152
 game_height = 648
 
 class Fish(pygame.sprite.Sprite):
-    def __init__(self,size,pos):
+    def __init__(self,pos):
         pygame.sprite.Sprite.__init__(self)
         # size is set by width then height
-        self.size = [size*2.2,size]
+        self.randomize_size = random.randint(10,100)
+        self.size = [self.randomize_size*2.2,self.randomize_size]
+        self.score = self.size[0] * self.size[1]
         self.image = pygame.Surface(self.size)
         self.image.fill((255, 0, 0))
         self.pos = pos
         self.rect = self.image.get_rect()
         self.rect.center = pos
+        self.speed = random.randint(1,10)
 
     def move_fish(self):
         if self.rect.x  <= 0 - self.rect.width:
             self.rect.x = game_width
             self.rect.y = random.randint(self.rect.height, (game_height-self.rect.height))
+            self.randomize_size = random.randint(10,100)
+            self.size = [self.randomize_size*2.2,self.randomize_size]
         else:
-            self.rect.x -= 50
+            self.rect.x -= self.speed
 
-        #self.rect.center = [self.rect.x,self.rect.y]
+class Shark(Fish):
+    def __init__(self,pos):
+        super().__init__(pos)
+        self.type = 'shark'
+        self.size = [400,150]
+        self.image = pygame.Surface(self.size)
+        self.image.fill((0, 0, 255))
+        self.speed = 30
+        self.show_wait = 0
+        self.show = False
+        self.randomSet = random.randint(1,2000)
 
-    def render(self):
-        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), self.radius)
-
+    def move_fish(self):
+        print('Random: ',self.randomSet)
+        if self.show_wait == self.randomSet:
+            self.show = True
+            self.show_wait = 0 
+        elif self.show == True and self.rect.x > (0-self.size[0]):
+            self.rect.x -= self.speed
+        elif self.show == True and self.rect.x <= (0-self.size[0]):
+            self.show = False
+            self.randomSet = random.randint(1,2000)
+            self.rect.x = game_width
+            self.rect.y = random.randint(self.rect.height, (game_height-self.rect.height))
+        else:
+            self.show_wait += 1
+        
 def main():
 
     pygame.init()
@@ -35,18 +62,24 @@ def main():
     background_image = pygame.image.load('My_images/2_game_background.png').convert_alpha()
     background_image = pygame.transform.scale(background_image, (game_width, game_height))
 
+
+
+
     pygame.display.set_caption('Dangerous Diving')
     clock = pygame.time.Clock()
 
     # Game initialization
     smallFish = []
+    shark = Shark([game_width+100,100])
     fish_group = pygame.sprite.Group()
 
-
-    for i in range(10):
+    for i in range(7):
+        randomStagger = random.randint(200,500)
         randomY = random.randint(0,game_height)
-        smallFish.append(Fish(10,[game_width,randomY]))
+        smallFish.append(Fish([game_width+randomStagger,randomY]))
         fish_group.add(smallFish[i])
+
+    fish_group.add(shark)
 
     stop_game = False
     while not stop_game:
@@ -58,6 +91,8 @@ def main():
         # Game logic
         for fish in smallFish:
             fish.move_fish()
+        
+        shark.move_fish()
 
         # Draw background
         screen.blit(background_image,[0,0])
