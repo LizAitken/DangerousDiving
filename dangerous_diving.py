068ed3b7276f10556a3f46eeb6ba734b1be68eee
RJ Salamanca <salamancaronito@gmail.com>
2019-04-25 21:19:22 -0400

    added jellyfish gif

--- dangerous_diving.py
+++ dangerous_diving.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import os, os.path
 
 GAME_WIDTH = 1152
 GAME_HEIGHT = 648
@@ -58,7 +59,42 @@ class Shark(Ocean):
             self.rect.y = random.randint(self.rect.height, (GAME_HEIGHT-self.rect.height))
         else:
             self.show_wait += 1
-        
+
+class Jellyfish(Ocean):
+    def __init__(self,image):
+        super().__init__(image)
+
+        self.size = [75,75]
+        self.images= []
+        self.images.append(pygame.image.load('My_images/jellyfish/jellyfish0.png'))
+        self.images.append(pygame.image.load('My_images/jellyfish/jellyfish1.png'))
+        self.images.append(pygame.image.load('My_images/jellyfish/jellyfish2.png'))
+        self.images.append(pygame.image.load('My_images/jellyfish/jellyfish3.png'))
+        self.images.append(pygame.image.load('My_images/jellyfish/jellyfish4.png'))
+        self.images.append(pygame.image.load('My_images/jellyfish/jellyfish5.png'))
+        self.images.append(pygame.image.load('My_images/jellyfish/jellyfish6.png'))
+
+        for i in range(len(self.images)):
+            self.images[i] = pygame.transform.scale(self.images[i], (self.size[0], self.size[1]))
+
+        print(self.images[0].get_buffer())
+        # for i in range(len([f for f in os.listdir(imagepath) if os.path.isfile(os.path.join(imagepath,f))])-1):
+        #     self.images.append(pygame.image.load('%sjellyfish%d.png' % (imagepath,i)))
+
+        self.index = 0
+        self.stagger = 0
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        if self.stagger == 3:
+            self.stagger = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+            self.image = self.images[self.index]
+        else:
+            self.stagger += 1
 def main():
 
     pygame.init()
@@ -67,6 +103,7 @@ def main():
     background_image = pygame.image.load('My_images/2_game_background.png').convert_alpha()
     background_image = pygame.transform.scale(background_image, (GAME_WIDTH, GAME_HEIGHT))
     shark_image = pygame.image.load('My_images/shark.png').convert_alpha()
+    jellyfish_image = pygame.image.load('My_images/shark.png').convert_alpha()
 
     # create all fish images
     fish_image = {}
@@ -78,7 +115,8 @@ def main():
     # Game initialization
     smallFish = []
     shark = Shark(shark_image)
- 
+    jelly = Jellyfish(jellyfish_image)
+
     fish_group = pygame.sprite.Group()
 
     for i in range(len(fish_image)):
@@ -86,6 +124,7 @@ def main():
         fish_group.add(smallFish[i])
 
     fish_group.add(shark)
+    fish_group.add(jelly)
 
     stop_game = False
     while not stop_game:
@@ -99,15 +138,15 @@ def main():
             fish.move_object()
         
         shark.move_object()
-
+        jelly.move_object()
         # Draw background
         screen.blit(background_image,[0,0])
 
         # Game display
+        jelly.update()
         fish_group.draw(screen)
 
         pygame.display.update()
-
     pygame.quit()
 
 if __name__ == '__main__':
